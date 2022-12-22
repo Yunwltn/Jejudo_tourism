c3237c3b0e3cb6956f545cabe4f5ef0cf1710ac9
Yunwltn <120348555+Yunwltn@users.noreply.github.com>
2022-12-22 11:04:53 +0900

    아이콘 삽입

--- jejuEDA.py
+++ jejuEDA.py
@@ -9,21 +9,21 @@ def run_jejuEDA_app() :
     tourism_df = pd.read_csv('jjtourism_2022.csv',index_col=0)
     restaurant_df = pd.read_csv('jjrestaurant_2022.csv',index_col=0)
 
-    st.subheader('사용한 데이터 프레임 확인')
+    st.subheader('사용한 데이터 프레임 확인 :open_file_folder:')
     st.write('제주관광공사 비짓제주 홈페이지 내 관광지 검색 로그데이터')
     st.dataframe(tourism_df)
     st.write('신한카드 제주도 내 음식점별 매출 변화량 데이터')
     st.dataframe(restaurant_df)
     st.subheader('')
 
-    st.subheader('기본 통계 데이터')
+    st.subheader('기본 통계 데이터 :clipboard:')
     st.write('제주관광공사 비짓제주 홈페이지 내 관광지 검색 로그데이터')
     st.dataframe(tourism_df.describe())
     st.write('신한카드 제주도 내 음식점별 매출 변화량 데이터')
     st.dataframe(restaurant_df.describe())
     st.subheader('')
 
-    st.subheader('최대 / 최소 데이터 확인하기')
+    st.subheader('최대 / 최소 데이터 확인하기 :bookmark_tabs:')
     st.write('제주관광공사 비짓제주 홈페이지 내 관광지 검색 로그데이터')
     st.dataframe(tourism_df.loc[tourism_df['전체조회수'] == tourism_df['전체조회수'].max()])
     st.dataframe(tourism_df.loc[tourism_df['전체조회수'] == tourism_df['전체조회수'].min()])
@@ -32,7 +32,7 @@ def run_jejuEDA_app() :
     st.dataframe(restaurant_df.loc[restaurant_df['3개월전체매출수'] == restaurant_df['3개월전체매출수'].min()])
     st.subheader('')
 
-    st.subheader('상관 관계 분석')
+    st.subheader('상관 관계 분석 :mag:')
     st.write('제주관광공사 비짓제주 홈페이지 내 관광지 검색 로그데이터')
     st.dataframe(tourism_df.corr())
     st.write('신한카드 제주도 내 음식점별 매출 변화량 데이터')
